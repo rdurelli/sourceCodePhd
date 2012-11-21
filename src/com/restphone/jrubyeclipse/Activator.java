e87bfc8f2021c445781976de4be04e928a114e98
Rafael Durelli <rafa.durelli@gmail.com>
2012-11-21 10:26:59 -0200

    removi a chama para o JRuby
    Importante lembrar que depois eu irei utilizar JRuby para o parser HTML.

--- src/com/restphone/jrubyeclipse/Activator.java
+++ src/com/restphone/jrubyeclipse/Activator.java
@@ -43,51 +43,54 @@ public class Activator extends AbstractUIPlugin {
   public void start(BundleContext context) throws Exception {
     super.start(context);
     
-    String update = "UPDATE PERSONS SET usuario = '   ', usuario2 = '   ' WHERE usuario3 = '   ' AND usuario2 = ' '  ;";
-    
-    System.out.println("vai vai vai va");
-    
-    ZqlParser parserSQL = new ZqlParser();
-    
-    parserSQL.initParser(new ByteArrayInputStream(update
-			.getBytes()));
-    
-    ZStatement st = parserSQL.readStatement();
-    
-	if (st instanceof ZUpdate) {
-		ZUpdate query = (ZUpdate) st;
-		
-		System.out.println("O nome da tabela Ž " + query.getTable());
-	}
+//    String update = "UPDATE PERSONS SET usuario = '   ', usuario2 = '   ' WHERE usuario3 = '   ' AND usuario2 = ' '  ;";
+//    
+//    System.out.println("vai vai vai va");
+//    
+//    ZqlParser parserSQL = new ZqlParser();
+//    
+//    parserSQL.initParser(new ByteArrayInputStream(update
+//			.getBytes()));
+//    
+//    ZStatement st = parserSQL.readStatement();
+//    
+//	if (st instanceof ZUpdate) {
+//		ZUpdate query = (ZUpdate) st;
+//		
+//		System.out.println("O nome da tabela Ž " + query.getTable());
+//	}
 	
 	Teste1 testCaller = new Teste1();
 	
 	testCaller.callParser();
     
-    TesteGroovyAndJRuby te = new TesteGroovyAndJRuby();
-    System.out.println(te.testeImprimi());
-    
-    
-    ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
-    ScriptEngine scriptEngine = scriptEngineManager.getEngineByExtension("rb");
-    
-    InputStream is = getBundle().getEntry("/jruby/sayHello.rb").openStream();
-    Reader reader = new InputStreamReader(is);
-    
-    scriptEngine.eval(reader);
-    Invocable invocableEngine = (Invocable)scriptEngine;
- if (invocableEngine != null) {
-//	 tesate t = new tesate();
-//	 invocableEngine.invokeFunction("somar", 1, 2);
-	 RubyArray array = (RubyArray)invocableEngine.invokeFunction("somar", 1, 2, "Durelli");
-	 for (Object object : array) {
-		System.out.println(object);
-		
-	}
-	 
-	 te.apreenderGroovy();
-// System.out.println(invocableEngine.invokeFunction("somar", 1, 2).getClass());
- }
+	
+	
+	//de suma import‰ncia quando eu tiver que utilizar para chamar Ruby ou Groovy no eclipse plugin
+//    TesteGroovyAndJRuby te = new TesteGroovyAndJRuby();
+//    System.out.println(te.testeImprimi());
+//    
+//    
+//    ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
+//    ScriptEngine scriptEngine = scriptEngineManager.getEngineByExtension("rb");
+//    
+//    InputStream is = getBundle().getEntry("/jruby/sayHello.rb").openStream();
+//    Reader reader = new InputStreamReader(is);
+//    
+//    scriptEngine.eval(reader);
+//    Invocable invocableEngine = (Invocable)scriptEngine;
+// if (invocableEngine != null) {
+////	 tesate t = new tesate();
+////	 invocableEngine.invokeFunction("somar", 1, 2);
+//	 RubyArray array = (RubyArray)invocableEngine.invokeFunction("somar", 1, 2, "Durelli");
+//	 for (Object object : array) {
+//		System.out.println(object);
+//		
+//	}
+//	 
+//	 te.apreenderGroovy();
+//// System.out.println(invocableEngine.invokeFunction("somar", 1, 2).getClass());
+// }
     
   }
 
