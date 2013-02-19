bb20db3e32537a7901c1158e268e38a27f232a79
Rafael Durelli <rafa.durelli@gmail.com>
2013-02-18 21:28:32 -0300

    Removendo conflito

--- src/com/br/actions/IdentifySQL.java
+++ src/com/br/actions/IdentifySQL.java
@@ -4,42 +4,32 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.codehaus.groovy.control.messages.Message;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IProject;
+
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 
-import org.eclipse.emf.ecore.resource.Resource;
-
-
-import org.eclipse.emf.ecore.resource.Resource;
 
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
-import org.eclipse.jdt.core.IJavaElement;
+
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.internal.core.PackageFragmentRoot;
+
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.text.ITextSelection;
+
 
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.ISelectionListener;
-import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.PlatformUI;
 
 
-import org.eclipse.ui.dialogs.ResourceSelectionDialog;
 
 import org.eclipse.ui.dialogs.ResourceSelectionDialog;
-
-import org.eclipse.ui.internal.Workbench;
 
 import com.br.databaseDDL.DataBase;
 import com.br.utils.CreateSQLModel;
